2bbaa92253ea171ae883407380de00e4039d88c2
mklinman <mklinman@uci.edu>
2020-01-26 21:41:02 -0800

    Website content loaded via JSON for ease

--- js/main.js
+++ js/main.js
@@ -8,36 +8,136 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var LikeButton = function (_React$Component) {
-	_inherits(LikeButton, _React$Component);
+var BlockContent = function (_React$Component) {
+	_inherits(BlockContent, _React$Component);
 
-	function LikeButton(props) {
-		_classCallCheck(this, LikeButton);
+	function BlockContent(props) {
+		_classCallCheck(this, BlockContent);
 
-		var _this = _possibleConstructorReturn(this, (LikeButton.__proto__ || Object.getPrototypeOf(LikeButton)).call(this, props));
-
-		_this.state = { liked: false };
-		return _this;
+		/*
+   * Expected props:
+   * string blockTitle
+   * string[] paragraphs
+   * bool last
+   */
+		return _possibleConstructorReturn(this, (BlockContent.__proto__ || Object.getPrototypeOf(BlockContent)).call(this, props));
 	}
 
-	_createClass(LikeButton, [{
+	_createClass(BlockContent, [{
 		key: "render",
 		value: function render() {
-			var _this2 = this;
-
-			if (this.state.liked) {
-				return "You liked this.";
+			var paragraphs = [];
+			for (var i = 0; i < this.props.paragraphs.length; i++) {
+				paragraphs.push(React.createElement("p", { key: this.props.blockTitle + i.toString(),
+					dangerouslySetInnerHTML: { __html: this.props.paragraphs[i] }
+				}));
 			}
-
+			var className = "block_content";
+			if (this.props.last) {
+				className += " last";
+			}
 			return React.createElement(
-				"button",
-				{ onClick: function onClick() {
-						return _this2.setState({ liked: true });
-					} },
-				"Like"
+				"div",
+				{ className: className },
+				React.createElement(
+					"h1",
+					null,
+					this.props.blockTitle
+				),
+				paragraphs
 			);
 		}
 	}]);
 
-	return LikeButton;
+	return BlockContent;
 }(React.Component);
+
+var SectionContent = function (_React$Component2) {
+	_inherits(SectionContent, _React$Component2);
+
+	function SectionContent(props) {
+		_classCallCheck(this, SectionContent);
+
+		/*
+   * Expected props:
+   * string sectionTitle
+   * {string blockTitle, string[] paragraphs}[] blocks
+   */
+		return _possibleConstructorReturn(this, (SectionContent.__proto__ || Object.getPrototypeOf(SectionContent)).call(this, props));
+	}
+
+	_createClass(SectionContent, [{
+		key: "render",
+		value: function render() {
+			var blocks = [];
+			for (var i = 0; i < this.props.blocks.length; i++) {
+				blocks.push(React.createElement(BlockContent, {
+					key: this.props.sectionTitle + i.toString(),
+					blockTitle: this.props.blocks[i].blockName,
+					paragraphs: this.props.blocks[i].paragraphs, last: i == this.props.blocks.length - 1
+				}));
+			}
+
+			return React.createElement(
+				"div",
+				{ className: "block_section" },
+				React.createElement(
+					"div",
+					{ className: "label_content" },
+					React.createElement(
+						"h1",
+						null,
+						this.props.sectionTitle
+					)
+				),
+				React.createElement("div", { className: "space" }),
+				blocks
+			);
+		}
+	}]);
+
+	return SectionContent;
+}(React.Component);
+
+var InternalContent = function (_React$Component3) {
+	_inherits(InternalContent, _React$Component3);
+
+	function InternalContent(props) {
+		_classCallCheck(this, InternalContent);
+
+		/*
+   * Expected props:
+   * {string sectionTitle, {string blockTitle, string[] paragraphs}[]}[] content
+   */
+		return _possibleConstructorReturn(this, (InternalContent.__proto__ || Object.getPrototypeOf(InternalContent)).call(this, props));
+	}
+
+	_createClass(InternalContent, [{
+		key: "render",
+		value: function render() {
+			var sections = [];
+			for (var i = 0; i < this.props.content.length; i++) {
+				sections.push(React.createElement(SectionContent, {
+					key: "section" + i.toString(),
+					sectionTitle: this.props.content[i].sectionName,
+					blocks: this.props.content[i].blocks
+				}));
+			}
+
+			return sections;
+		}
+	}]);
+
+	return InternalContent;
+}(React.Component);
+
+//Load the content from the JSON file
+
+
+$.ajax({
+	dataType: "json",
+	url: "../assets/blocks.json",
+	success: function success(data) {
+		ReactDOM.render(React.createElement(InternalContent, { content: data }), document.getElementById('internal_content'));
+	}
+});
--- src/main.js
+++ src/main.js
@@ -1,21 +1,102 @@
 'use strict';
 
-class LikeButton extends React.Component {
+class BlockContent extends React.Component {
 	constructor(props) {
+		/*
+		 * Expected props:
+		 * string blockTitle
+		 * string[] paragraphs
+		 * bool last
+		 */
 		super(props);
-		this.state = { liked: false };
 	}
 
 	render() {
-		if (this.state.liked) {
-			return "You liked this.";
+		let paragraphs = [];
+		for (let i = 0; i < this.props.paragraphs.length; i++) {
+			paragraphs.push(
+				<p key={this.props.blockTitle + i.toString()}
+					dangerouslySetInnerHTML={{__html: this.props.paragraphs[i]}}
+				>
+				</p>
+			);
 		}
-
+		let className = "block_content";
+		if (this.props.last) {
+			className += " last";
+		}
 		return (
-			<button onClick={() => this.setState({ liked: true })}>
-				Like
-			</button>
+			<div className={className}>
+				<h1>{this.props.blockTitle}</h1>
+				{paragraphs}
+			</div>
 		);
 	}
 }
+
+class SectionContent extends React.Component {
+	constructor(props) {
+		/*
+		 * Expected props:
+		 * string sectionTitle
+		 * {string blockTitle, string[] paragraphs}[] blocks
+		 */
+		super(props);
+	}
+
+	render() {
+		let blocks = [];
+		for (let i = 0; i < this.props.blocks.length; i++) {
+			blocks.push(
+			<BlockContent 
+				key={this.props.sectionTitle + i.toString()}
+				blockTitle={this.props.blocks[i].blockName}
+				paragraphs={this.props.blocks[i].paragraphs} last={i == this.props.blocks.length - 1}
+			/>); 
+		}
+
+		return (
+			<div className="block_section">	
+				<div className="label_content">
+					<h1>{this.props.sectionTitle}</h1>
+				</div>
+				<div className="space"></div>
+				{blocks}
+			</div>
+		);
+	}
+}
+
+class InternalContent extends React.Component {
+	constructor(props) {
+		/*
+		 * Expected props:
+		 * {string sectionTitle, {string blockTitle, string[] paragraphs}[]}[] content
+		 */
+		super(props);
+	}
+
+	render() {
+		let sections = [];
+		for (let i = 0; i < this.props.content.length; i++) {
+			sections.push(
+			<SectionContent 
+				key={"section" + i.toString()}
+				sectionTitle={this.props.content[i].sectionName}
+				blocks={this.props.content[i].blocks}
+			/>);
+		}
+
+		return sections;
+	}
+}
+
+//Load the content from the JSON file
+$.ajax({
+	dataType: "json",
+	url: "../assets/blocks.json",
+	success: function(data) {	
+		ReactDOM.render(<InternalContent content={data}/>, document.getElementById('internal_content'));
+	}
+});
 
